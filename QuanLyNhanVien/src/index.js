005155b3f95c5c3f196f75aa4c827a46a20118ce
Dev1 <hoangphuc2420@gmail.com>
2023-10-01 11:20:16 +0700

    push

--- QuanLyNhanVien/src/index.js
+++ QuanLyNhanVien/src/index.js
@@ -13,6 +13,54 @@ function layThongTinNhanVien() {
   var _chucVu = getele("chucvu").value;
   var _gioLam = getele("gioLam").value * 1;
   // Validation
+  var isValid = true;
+  // kiểm tra ngày
+  isValid &=
+    kiemTraRong(_ngayLam, "tbNgay", "Vui lòng không để trống") &&
+    dinhDangNgay(_ngayLam, "tbNgay", "Vui lòng nhập đúng định dạng dd/mm/yyyy");
+
+  // Kiểm tra tài khoản
+  isValid &=
+    kiemTraRong(_tk, "tbTKNV", "Vui lòng không để trống") &&
+    kiemTraDoDaiKyTu(_tk, 4, 6, "tbTKNV", "Vui lòng nhập từ 4 đến 6 ký tự ");
+  // kiểm tra tên
+  isValid &=
+    kiemTraRong(_hoTen, "tbTen", "Vui lòng không để trống") &&
+    kiemTraKyTu(_hoTen, "tbTen", "Vui lòng không nhập số");
+  // kiểm tra email
+  isValid &=
+    kiemTraRong(_email, "tbEmail", "Vui lòng không để trống") &&
+    kiemTraEmail(_email, "tbEmail", "Vui lòng nhập đúng Email");
+  // kiểm tra mật khẩu
+  isValid &=
+    kiemTraRong(_mk, "tbMatKhau", "Vui lòng không để trống") &&
+    kiemTraDoDaiKyTu(
+      _mk,
+      6,
+      10,
+      "tbMatKhau",
+      "Vui lòng nhập từ 6 đến 10 ký tự"
+    ) &&
+    kiemTraPass(
+      _mk,
+      "tbMatKhau",
+      "Vui lòng nhập có chứa ít nhất 1 số , 1 ký tự in hoa , 1 ký tự đặc biệt"
+    );
+  // kiểm tra lương
+  isValid &=
+    kiemTraRong(_luong, "tbLuongCB", "Vui lòng không để trống") &&
+    kiemTraSo(_luong, "tbLuongCB", "Vui lòng nhập số") &&
+    kiemLuong(_luong, 1000000, 20000000, "tbLuongCB", "Nhập từ 1tr đến 20tr");
+  // kiểm tra chức vụ
+  isValid &= kiemTraChucVu("chucvu", "tbChucVu", "Vui lòng chọn chức vụ");
+  // kiểm tra giờ làm
+  isValid &= kiemGioLam(
+    _gioLam,
+    80,
+    200,
+    "tbGiolam",
+    "Nhập từ 80 đến 200 tiếng"
+  );
 
   var nv = new NhanVien(
     _tk,
@@ -30,8 +78,10 @@ function layThongTinNhanVien() {
 }
 function themNhanVien() {
   var nv = layThongTinNhanVien();
-  dsnv._themNV(nv);
-  renderListNV(dsnv.arr);
+  if (nv) {
+    dsnv._themNV(nv);
+    renderListNV(dsnv.arr);
+  }
 }
 
 function renderListNV(data) {
@@ -39,15 +89,15 @@ function renderListNV(data) {
   for (var i = 0; i < data.length; i++) {
     var nv = data[i];
     content += `
-    <tr>
-    <td>${nv.tk}</td>
-    <td>${nv.hoTen}</td>
-    <td>${nv.email}</td>
-    <td>${nv.ngayLam}</td>
-    <td>${nv.chucVu}</td>
-    <td>${nv.tongLuong}</td>
-    <td>${nv.xepLoai}</td>
-    </tr>
+        <tr>
+        <td>${nv.tk}</td>
+        <td>${nv.hoTen}</td>
+        <td>${nv.email}</td>
+        <td>${nv.ngayLam}</td>
+        <td>${nv.chucVu}</td>
+        <td>${nv.tongLuong}</td>
+        <td>${nv.xepLoai}</td>
+        </tr>
     `;
   }
   getele("tableDanhSach").innerHTML = content;
--- QuanLyNhanVien/src/validation.js
+++ QuanLyNhanVien/src/validation.js
@@ -8,10 +8,23 @@ function kiemTraRong(value, idCanhBao, mess) {
     return true;
   }
 }
+function kiemTraDoDaiKyTu(value, min, max, idCanhBao, mess) {
+  var domSpan = document.getElementById(idCanhBao);
+
+  if (value.length < min || value.length > max) {
+    domSpan.innerHTML = mess;
+    return false;
+  } else {
+    domSpan.innerHTML = "";
+    return true;
+  }
+}
+
 function kiemTraKyTu(value, idCanhBao, mess) {
   var domSpan = document.getElementById(idCanhBao);
-  var regexString = /ab+c/;
-  var isValid = regexString.test(hoTen);
+  var regexString =
+    /[^a-z0-9A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]/u;
+  var isValid = regexString.test(value);
   if (isValid == true) {
     domSpan.innerHTML = "";
     return true;
@@ -27,8 +40,82 @@ function kiemTraEmail(email, idCanhBao, mess) {
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
   var isValid = regexEmail.test(email);
-  console.log(isValid);
+
   if (isValid == true) {
+    domSpan.innerHTML = "";
+    return true;
+  } else {
+    domSpan.innerHTML = mess;
+    return false;
+  }
+}
+function kiemTraPass(pass, idCanhBao, mess) {
+  var domSpan = document.getElementById(idCanhBao);
+  var regexPass =
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{6,10}$/;
+
+  var isValid = regexPass.test(pass);
+  if (isValid == true) {
+    domSpan.innerHTML = "";
+    return true;
+  } else {
+    domSpan.innerHTML = mess;
+    return false;
+  }
+}
+function kiemLuong(value, min, max, idCanhBao, mess) {
+  var domSpan = document.getElementById(idCanhBao);
+  if (value < min || value > max) {
+    // sai
+    domSpan.innerHTML = mess;
+    return false;
+  } else {
+    domSpan.innerHTML = "";
+    return true;
+  }
+}
+function kiemTraChucVu(idSelect, idCanhBao, mess) {
+  var domSpan = document.getElementById(idCanhBao);
+  var domSelect = document.getElementById(idSelect).selectedIndex;
+  if (domSelect == 0) {
+    domSpan.innerHTML = mess;
+    return false;
+  } else {
+    domSpan.innerHTML = "";
+    return true;
+  }
+}
+function kiemGioLam(value, min, max, idCanhBao, mess) {
+  var domSpan = document.getElementById(idCanhBao);
+  if (value < min || value > max) {
+    domSpan.innerHTML = mess;
+    return false;
+  } else {
+    domSpan.innerHTML = "";
+    return true;
+  }
+}
+function dinhDangNgay(value, idCanhBao, mess) {
+  var domSpan = document.getElementById(idCanhBao);
+  var regexDate =
+    /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+  var isValid = regexDate.test(value);
+  if (isValid == true) {
+    domSpan.innerHTML = "";
+    return true;
+  } else {
+    domSpan.innerHTML = mess;
+    return false;
+  }
+}
+function kiemTraSo(value, idCanhBao, mess) {
+  var domSpan = document.getElementById(idCanhBao);
+
+  var regexNumber = /^\d+$/;
+
+  var isValid = regexNumber.test(value);
+  console.log(isValid);
+  if (isValid) {
     domSpan.innerHTML = "";
     return true;
   } else {
